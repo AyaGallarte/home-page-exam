5bdfd309a5fef44c5cdfcfb2966a8834536c5b52
aya.gallarte <aya.gallarte@gmail.com>
2024-08-01 14:35:37 +0800

    modified main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,3 @@
-// import { render } from 'preact'
-// import { App } from './app.jsx'
-// import './index.css'
-
-// render(<App />, document.getElementById('app'))
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './app.jsx'
